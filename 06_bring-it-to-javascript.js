0559fc08797ae990fd3e5e41b832bf733c7d67b8
jetwat <jetwatwarathip@gmail.com>
2026-03-10 16:54:25 +0700

    updated comment in file 06

--- 06_bring-it-to-javascript.js
+++ 06_bring-it-to-javascript.js
@@ -40,5 +40,5 @@ const supportTicket = {
   status: "Open"
 };
 
-// Log to console
+// Log
 console.log(`ลูกค้าชื่อ: ${customer.name} (โทร: ${customer.phone}) แจ้งเรื่องเข้ามาว่า: "${supportTicket.subject}"`);
